85174e9b95ce1d51a6f187279ab450e8e1f2bc7f
Ajay Pratap Singh <ajaysingh91400@gmail.com>
2026-05-20 18:45:36 +0530

    fixes by ajay

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import { connectDB } from './config/db';
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 const startServer = async () => {
   await connectDB();
